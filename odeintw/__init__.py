84f7ddd076f08878b2e671bf3e98f396d0989c75
Warren Weckesser <warren.weckesser@gmail.com>
2019-04-09 01:30:59 -0400

    Add __version__ to the odeintw module.

--- odeintw/__init__.py
+++ odeintw/__init__.py
@@ -6,4 +6,7 @@ from numpy.testing import Tester as _Tester
 
 from ._odeintw import odeintw
 
+
+__version__ = "0.0.2.dev1"
+
 test = _Tester().test
--- setup.py
+++ setup.py
@@ -4,14 +4,33 @@
 # All rights reserved.
 # See the LICENSE file for license information.
 
+from os import path
 from distutils.core import setup
+
+
+def get_odeintw_version():
+    """
+    Find the value assigned to __version__ in odeintw/__init__.py.
+
+    This function assumes that there is a line of the form
+
+        __version__ = "version-string"
+
+    in odeintw/__init__.py.  It returns the string version-string, or None if such a
+    line is not found.
+    """
+    with open(path.join("odeintw", "__init__.py"), "r") as f:
+        for line in f:
+            s = [w.strip() for w in line.split("=", 1)]
+            if len(s) == 2 and s[0] == "__version__":
+                return s[1][1:-1]
 
 
 _descr = ('Solve complex and matrix differential equations '
           'with scipy.integrate.odeint.')
 
 setup(name='odeintw', 
-      version='0.0.1',
+      version=get_odeintw_version(),
       description=_descr,
       author='Warren Weckesser',
       url='https://github.com/WarrenWeckesser/odeintw',
