bcdf39e00e3c444344abc4785336a43cf1c61c75
Warren Weckesser <warren.weckesser@gmail.com>
2020-11-02 16:12:29 -0500

    MAINT: Don't use yield tests.

--- odeintw/tests/test_odeintw.py
+++ odeintw/tests/test_odeintw.py
@@ -24,7 +24,7 @@ C = np.array([[-20+1j, 5-1j,      0,       0],
 def test_complex_to_real_jac():
     z = np.array([[1+2j]])
     r = _complex_to_real_jac(z)
-    yield assert_array_equal, r, np.array([[1, -2], [2, 1]])
+    assert_array_equal(r, np.array([[1, -2], [2, 1]]))
     z = np.array([[1+2j, 3+4j],
                   [5+6j, 7+8j]])
     r = _complex_to_real_jac(z)
@@ -32,7 +32,7 @@ def test_complex_to_real_jac():
                          [2,  1, 4,  3],
                          [5, -6, 7, -8],
                          [6,  5, 8,  7]])
-    yield assert_array_equal, r, expected
+    assert_array_equal(r, expected)
 
 
 @dec.knownfailureif(_scipy_version < (0, 14, 0), _banded_bug_msg) 
@@ -47,7 +47,7 @@ def test_transform_banded_jac():
                          [0,  6,  3,  8],
                          [5, 10,  7, 12],
                          [9,  0, 11,  0]])
-    yield assert_array_equal, t, expected
+    assert_array_equal(t, expected)
 
 
 def system1_complex(z, t):
@@ -65,7 +65,7 @@ def test_odeintw_complex():
     zsol = odeintw(system1_complex, z0, t)
     zv = zsol.view(np.float64)
     sol = odeintw(system1_real, [z0.real, z0.imag], t)
-    yield assert_allclose, zv, sol
+    assert_allclose(zv, sol)
 
 
 def system2_array(a, t):
@@ -83,7 +83,7 @@ def test_odeint_array():
     asol = odeintw(system2_array, a0, t)
     avec = asol.reshape(-1, 4)
     sol = odeintw(system2_vector, a0.ravel(), t)
-    yield assert_allclose, avec, sol
+    assert_allclose(avec, sol)
 
 
 def system3_func(y, t, c):
@@ -132,8 +132,8 @@ def test_system3():
     # been defined so the call to odeint in odeintw follows the same
     # code path as the call to odeint above.  Still, floating point operations
     # aren't necessarily deterministic.
-    yield assert_array_equal, sol0.view(np.float64), sol1
-    yield assert_array_equal, nje0, nje1
+    assert_array_equal(sol0.view(np.float64), sol1)
+    assert_array_equal(nje0, nje1)
 
 
 @dec.knownfailureif(_scipy_version < (0, 14, 0), _banded_bug_msg) 
@@ -156,12 +156,12 @@ def test_system3_banded():
     sol2, info2 = odeintw(system3_func, z0, t, Dfun=system3_bjac_rows,
                           ml=0, mu=1, **common_kwargs)
 
-    yield assert_allclose, sol0, sol1
-    yield assert_allclose, sol0, sol2
+    assert_allclose(sol0, sol1)
+    assert_allclose(sol0, sol2)
     # The same code paths should have been followed in computing
     # sol1 and sol2, so the number of jacobian evaluations should
     # be the same.
-    yield assert_array_equal, info1['nje'], info2['nje']
+    assert_array_equal(info1['nje'], info2['nje'])
 
 
 def system1_complex_tfirst(t, z):
